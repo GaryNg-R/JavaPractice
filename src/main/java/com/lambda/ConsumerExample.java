dd80c0e7a1a6e1359550da5def9de6a903c6f500
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-13 17:30:22 -0400

    try functionalinterface consumer .accept

---
+++ src/main/java/com/lambda/ConsumerExample.java
@@ -0,0 +1,13 @@
+package com.lambda;
+
+import java.util.function.Consumer;
+
+//FuncationalInterface
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        Consumer<String> c1 = (s) -> System.out.println(s.toUpperCase());
+
+        c1.accept("testing");
+    }
+}
---
+++ src/main/java/com/lambda/local.java
@@ -0,0 +1,41 @@
+package com.lambda;
+
+import java.util.*;
+
+public class local {
+
+
+    public static String gridChallenge(List<String> grid) {
+        List<String> temp = new ArrayList<>();
+        for(int i = 0; i < grid.size(); i ++){
+            char charArr[] = grid.get(i).toCharArray();
+            Arrays.sort(charArr);
+            temp.add(String.valueOf(charArr));
+        }
+
+
+        for(int i = 1; i < temp.size(); i ++) {
+            for(int j = 0; j < temp.size(); j ++){
+
+                if(temp.get(i-1).charAt(j) > temp.get(i).charAt(j)){
+
+                    return "No";
+                }
+            }
+           // System.out.println("one round");
+        }
+
+        return "Yes";
+    }
+
+    public static void main(String[] args) {
+        List<String> grid = List.of(new String[]{"eabcd", "fghij", "olkmn", "trpqs", "xywuv"});
+        System.out.println(gridChallenge(grid));
+    }
+}
+
+/*
+*  abc
+*  ade
+*  efg
+* */
