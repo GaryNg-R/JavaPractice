0309fbce3a23c243141c8f87ae40b75611473264
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-22 15:11:30 -0400

    using Bipredicate

--- src/main/java/com/funtionallnterfaces/PredicateAndConsumerExample.java
+++ src/main/java/com/funtionallnterfaces/PredicateAndConsumerExample.java
@@ -5,6 +5,7 @@ import com.data.StudentDataBase;
 
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.BiPredicate;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 
@@ -12,6 +13,8 @@ public class PredicateAndConsumerExample {
 
     Predicate<Student> p1 = (student) -> student.getGradeLevel() >=3;
     Predicate<Student> p2 = (student) -> student.getGpa() >=3.9;
+
+    BiPredicate<Integer,Double> biPredicate = (gradeLevel, gpa) -> gradeLevel >= 3 && gpa >=3.9;
 
     BiConsumer<String, List<String>> studentBiConsumer = (name,activities) -> System.out.println(name + " : " + activities);;
 
