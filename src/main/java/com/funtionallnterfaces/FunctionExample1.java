41de11015ac3313d91f0df3247735e32aeeee628
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-07-28 21:58:43 -0400

    add functionExample1

--- src/main/java/com/funtionallnterfaces/FunctionExample1.java
+++ src/main/java/com/funtionallnterfaces/FunctionExample1.java
@@ -1,4 +1,13 @@
 package com.funtionallnterfaces;
 
 public class FunctionExample1 {
+
+    public static String performConcat (String str){
+        return FunctionExample.addSomeString.apply(str);
+    }
+
+    public static void main(String[] args) {
+        String result = performConcat("Hello");
+        System.out.println("Result = " + result);
+    }
 }
