ee11f98fb303ef74453bef661d7c5620d989d3c9
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-08-04 13:27:40 -0400

    add refactor

--- src/main/java/com/methodreference/ConsumerMethodReferenceExample.java
+++ src/main/java/com/methodreference/ConsumerMethodReferenceExample.java
@@ -1,8 +1,20 @@
 package com.methodreference;
 
+import com.data.Student;
+import com.data.StudentDataBase;
+
+import java.util.function.Consumer;
+
 public class ConsumerMethodReferenceExample {
+
+    static Consumer<Student> c1= System.out::println;
+
+    static Consumer<Student> c2 = Student::printListOfActivities;
 
     public static void main(String[] args) {
 
+        //StudentDataBase.getAllStudents().forEach(c1);
+
+        StudentDataBase.getAllStudents().forEach(c2);
     }
 }
