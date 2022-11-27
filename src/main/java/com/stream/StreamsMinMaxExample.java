be43d7d66eeae84a51207797e5062ca04653ce60
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-11-27 11:42:49 -0800

    Max example

--- src/main/java/com/stream/StreamsMinMaxExample.java
+++ src/main/java/com/stream/StreamsMinMaxExample.java
@@ -2,6 +2,7 @@ package com.stream;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 
 public class StreamsMinMaxExample {
 
@@ -13,11 +14,31 @@ public class StreamsMinMaxExample {
                 //9...
                 //x variable hold the max value for each element in the iteration
                 .reduce(0,(x,y) -> x>y ? x:y);
+                //should not 0 as default value
     }
 
+    public static Optional<Integer> findMaxValueOptional(List<Integer> integerList){
+        return integerList.stream()
+                //6 ->y
+                //7 ->y
+                //8
+                //9...
+                //x variable hold the max value for each element in the iteration
+                .reduce((x,y) -> x>y ? x:y);
+        //should not 0 as default value use Optional
+    }
     public static void main(String[] args) {
         List<Integer> integerList = Arrays.asList(6,7,8,9,10);
         int maxValue = findMaxValue(integerList);
         System.out.println("Max value is : " + maxValue);
+
+        List<Integer> integerListOptional = Arrays.asList();
+        Optional<Integer> maxValueOptional = findMaxValueOptional(integerListOptional);
+        if(maxValueOptional.isPresent()){
+            System.out.println("MaxVaUle using optional :" + maxValueOptional.get());
+        }else{
+            System.out.println("Input list is empty");
+        }
+
     }
 }
