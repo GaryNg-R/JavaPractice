d7eee2b5f2154408cf7160b28d88c19910ecc809
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-04 11:12:14 -0800

    add skip

--- src/main/java/com/stream/StreamLimitSkipExample.java
+++ src/main/java/com/stream/StreamLimitSkipExample.java
@@ -14,11 +14,21 @@ public class StreamLimitSkipExample {
                 //9
                 //10
                 .limit(2)
-                //only 6,7 passin
+                //only 6,7 passing
                 .reduce((x,y)-> x+y);
     }
 
-
+    public static Optional<Integer> skip(List<Integer> integers){
+        return integers.stream()
+                //6
+                //7
+                //8
+                //9
+                //10
+                .skip(3)
+                //only 9,10 passing
+                .reduce((x,y)-> x+y);
+    }
     public static void main(String[] args) {
         List<Integer> integers = Arrays.asList(6,7,8,9,10);
         Optional<Integer> limitResult = limit(integers);
