dc65c5da42704302d3f4b878339c25995c1b04c8
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-04 11:11:00 -0800

    using limit and add too using reduce

---
+++ src/main/java/com/stream/StreamLimitSkipExample.java
@@ -0,0 +1,31 @@
+package com.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class StreamLimitSkipExample {
+
+    public static Optional<Integer> limit(List<Integer> integers){
+        return integers.stream()
+                //6
+                //7
+                //8
+                //9
+                //10
+                .limit(2)
+                //only 6,7 passin
+                .reduce((x,y)-> x+y);
+    }
+
+
+    public static void main(String[] args) {
+        List<Integer> integers = Arrays.asList(6,7,8,9,10);
+        Optional<Integer> limitResult = limit(integers);
+        if(limitResult.isPresent()){
+            System.out.println("The limit result is :"+ limitResult.get());
+        }else{
+            System.out.println("No input is passed");
+        }
+    }
+}
