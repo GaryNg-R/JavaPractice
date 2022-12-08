031301f4e187e571cd1f188aecf1652458bb65c1
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-08 09:58:40 -0800

    add anyMatch and noneMatch

--- src/main/java/com/stream/StreamMatchExample.java
+++ src/main/java/com/stream/StreamMatchExample.java
@@ -6,10 +6,25 @@ public class StreamMatchExample {
 
     public static boolean allMatch(){
         return StudentDataBase.getAllStudents().stream()
-                .allMatch(student -> student.getGpa() >= 3.9);
+                .allMatch(student -> student.getGpa() >= 3.8);
+    }
+
+
+    public static boolean anyMatch(){
+        return StudentDataBase.getAllStudents().stream()
+                .anyMatch(student -> student.getGpa() >= 4.0);
+    }
+
+
+    public static boolean noneMatch(){
+        return StudentDataBase.getAllStudents().stream()
+                .noneMatch(student -> student.getGpa() >= 4.0);
     }
 
     public static void main(String[] args) {
         System.out.println("Result of All Match :" +allMatch());
+        System.out.println("Result of Any Match :" +anyMatch());
+        System.out.println("Result of None Match :" +noneMatch());
+
     }
 }
