3b54f677fb54189238b406b6498c94fe0e1d0f68
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-12 13:15:30 -0800

    rangeClosed

--- src/main/java/com/numericstreams/NumericStreamAggregatexample.java
+++ src/main/java/com/numericstreams/NumericStreamAggregatexample.java
@@ -7,5 +7,6 @@ public class NumericStreamAggregatexample {
     public static void main(String[] args) {
        int sum =  IntStream.rangeClosed(1,50).sum();
         System.out.println("Sum is " + sum);
+
     }
 }
