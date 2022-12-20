993e5b3f603a0e6f029ec929cde7ec934395128a
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-20 13:45:17 -0800

    add long and aver

--- src/main/java/com/numericstreams/NumericStreamAggregatexample.java
+++ src/main/java/com/numericstreams/NumericStreamAggregatexample.java
@@ -1,7 +1,10 @@
 package com.numericstreams;
 
+import java.util.OptionalDouble;
 import java.util.OptionalInt;
+import java.util.OptionalLong;
 import java.util.stream.IntStream;
+import java.util.stream.LongStream;
 
 public class NumericStreamAggregatexample {
 
@@ -11,5 +14,16 @@ public class NumericStreamAggregatexample {
 
         OptionalInt max =  IntStream.rangeClosed(1,50).max();
         System.out.println("Max is " + (max.isPresent() ? max.getAsInt() : 0));
+
+        OptionalLong optionLong = LongStream.rangeClosed(50,100).min();
+
+        System.out.println("Min is " + (optionLong.isPresent() ? optionLong.getAsLong() : 0));
+
+        OptionalDouble optionDouble = IntStream.rangeClosed(1,50).average();
+
+        System.out.println("Average is " + (optionDouble.isPresent() ? optionDouble.getAsDouble() : 0));
+
+
+
     }
 }
