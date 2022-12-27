c2c029e9a94f89e462f461e63753b9aaf423df3d
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-27 11:43:18 -0800

    groupby 2level

--- src/main/java/com/streams_terminal/StreamsGroupingByExample.java
+++ src/main/java/com/streams_terminal/StreamsGroupingByExample.java
@@ -7,6 +7,7 @@ import java.util.List;
 import java.util.Map;
 
 import static java.util.stream.Collectors.groupingBy;
+import static java.util.stream.Collectors.summingInt;
 
 public class StreamsGroupingByExample {
 
@@ -24,8 +25,26 @@ public class StreamsGroupingByExample {
         System.out.println(studentMap);
     }
 
+    public static void twoLeveGrouping_1(){
+        Map<Integer, Map<String, List<Student>>> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        groupingBy(student -> student.getGpa() >= 3.8 ? "OUTSTANDING" : "AVERAGE" )));
+        System.out.println(studentMap);
+    }
+
+    public static void twoLeveGrouping_2(){
+        Map<String,Integer> studentMap = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getName,
+                        summingInt(Student::getNoteBooks)));
+        System.out.println(studentMap);
+    }
+
     public static void main(String[] args) {
         //groupStudentByGender();
-        customizedGroupingBy();
+        //customizedGroupingBy();
+        //twoLeveGrouping_1();
+        twoLeveGrouping_2();
     }
 }
