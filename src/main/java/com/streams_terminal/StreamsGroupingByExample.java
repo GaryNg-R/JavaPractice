cfcfcd5f6110e40b8e2babbbdecb5e1fdcfa4550
Gary Ng <garyng@garys-mbp.attlocal.net>
2022-12-28 09:35:35 -0800

    add grouping example

--- src/main/java/com/streams_terminal/StreamsGroupingByExample.java
+++ src/main/java/com/streams_terminal/StreamsGroupingByExample.java
@@ -3,11 +3,9 @@ package com.streams_terminal;
 import com.data.Student;
 import com.data.StudentDataBase;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static java.util.stream.Collectors.groupingBy;
-import static java.util.stream.Collectors.summingInt;
+import static java.util.stream.Collectors.*;
 
 public class StreamsGroupingByExample {
 
@@ -41,10 +39,49 @@ public class StreamsGroupingByExample {
         System.out.println(studentMap);
     }
 
+    public static void threeArgumentGroupBy(){
+        LinkedHashMap<String, Set<Student>> studentLinkedHashMap =  StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getName,
+                        LinkedHashMap::new,toSet()));
+        System.out.println(studentLinkedHashMap);
+    }
+
+
+    public static void calculateTopGpa(){
+        Map<Integer, Optional<Student>> studentMapOptional = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        maxBy(Comparator.comparing(Student::getGpa))));
+        System.out.println(studentMapOptional);
+    }
+
+    public static void calculateTopGpa2(){
+        Map<Integer, Student> studentMapOptional = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        collectingAndThen(maxBy(Comparator.comparing(Student::getGpa))
+                        ,Optional::get)));
+        System.out.println(studentMapOptional);
+
+
+        Map<Integer, Student> studentMapOptional2 = StudentDataBase.getAllStudents()
+                .stream()
+                .collect(groupingBy(Student::getGradeLevel,
+                        collectingAndThen(minBy(Comparator.comparing(Student::getGpa))
+                                ,Optional::get)));
+        System.out.println(studentMapOptional2);
+    }
+
     public static void main(String[] args) {
         //groupStudentByGender();
         //customizedGroupingBy();
         //twoLeveGrouping_1();
-        twoLeveGrouping_2();
+        //twoLeveGrouping_2();
+        //threeArgumentGroupBy();
+        calculateTopGpa2();
+
+
+
     }
 }
